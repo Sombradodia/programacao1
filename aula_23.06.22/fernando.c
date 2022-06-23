0d7ba24fc49d0256cde99ebcfefbb415b653ce10
Sombradodia <106778941+Sombradodia@users.noreply.github.com>
2022-06-23 19:56:56 -0300

    Create fernando.c

---
+++ aula_23.06.22/fernando.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+int j;
+printf("sao duvisiveis por 3:");
+i=1
+while (i <=40)
+{
+if (i%3==0)
+printf("%d\n",i);
+i++;
+
+	return 0;
+} 
+
+#include <stdio.h>
+int main()
+{
+char opcao;
+printf("escolha uma opção:\n");
+printf("a) 10\n");
+printf("b) 20\n");
+printf("c) 30\n");
+scanf("%c",&opcao);
+printf("tua opção:%c\n",opcao);
+}
